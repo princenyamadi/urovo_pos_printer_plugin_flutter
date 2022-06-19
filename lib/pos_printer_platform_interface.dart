f6f6f56f7e76ff0d2e6b1864bf7e105ef6fc731e
= <=>
2022-06-19 03:50:54 +0000

    🚀 added added platform interface

--- lib/pos_printer_platform_interface.dart
+++ lib/pos_printer_platform_interface.dart
@@ -1,3 +1,5 @@
+
+import 'package:bitmap/bitmap.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'pos_printer_method_channel.dart';
@@ -26,4 +28,180 @@ abstract class PosPrinterPlatform extends PlatformInterface {
   Future<String?> getPlatformVersion() {
     throw UnimplementedError('platformVersion() has not been implemented.');
   }
+
+  Future<String?> print(){
+    throw UnimplementedError('print() has not been implemented.');
+  }
+  /// gets printers status
+  Future<int?> getStatus(){
+    throw UnimplementedError('getStatus() has not been implemented');
+  }
+  /// close printer
+  Future<int?> dispose(){
+    throw UnimplementedError('dispose() has not been implemented');
+  }
+  /// sets printer up for printing
+  Future<int?> setupPage({required int height, required int width}){
+    throw UnimplementedError('setupPage() has not been implemented');
+  }
+  /// set gray level for printer
+  Future<int?> setGrayLevel(int level){
+    throw UnimplementedError('setGrayLevel(int level) has not been implemented');
+  }
+  /// length of paper in printer
+  Future<int?> paperFeed(int length){
+    throw UnimplementedError('paperFeed(int length) has not been implemented');
+  }
+
+  /// speed of priner
+  /// ranges from 0 to 9
+  Future<int?> setSpeedLevel(int level){
+    throw UnimplementedError('setSpeedLevel(int level) has not been implemented');
+  }
+
+  ///resets page setup or clears page set up
+  Future<int?> clearPage(){
+    throw UnimplementedError('clearPage() has not been implemented');
+  }
+
+  /// print page
+
+  Future<int?> printPage(int rotate){
+    throw UnimplementedError('printPage(int rotate) has not been implemented');
+  }
+  /// drawline
+  Future<int?> drawLine({required int x0,required int  y0, required int x1,required int y1, required int lineWidth}){
+    throw UnimplementedError('drawLine({required int x0,required int  y0, required int x1,required int y1, required int lineWidth}) has not been implemented');
+  }
+  /// drawText
+  Future<int?> drawText({required String data, required int x, required int y, required String fontName, required bool isBold, required bool isItalic, required int rotate }){
+    throw UnimplementedError('drawText({required String data, required int x, required int y, required String fontName, required bool isBold, required bool isItalic, required int rotate }) has not been implemented');
+  }
+
+  /// drawTextEx
+  Future<int?> drawTextEx({required String data, required int x, required int y,required int width, required int height, required String fontName,required int fontSize, required bool isBold, required bool isItalic, required int rotate, required int style,required int format }){
+    throw UnimplementedError('drawTextEx({required String data, required int x, required int y,required int width, required int height, required String fontName,required int fontSize, required bool isBold, required bool isItalic, required int rotate, required int style,required int format }) has not been implemented');
+  }
+
+/// drawBarcode
+Future<int?> drawBarcode({required String data, required int x, required int y, required int barcodeType, required int width, required int height, required int rotate}){
+  throw UnimplementedError('drawBarcode({required String data, required int x, required int y, required int barcodeType, required int width, required int height, required int rotate}) has not been implemented');
+
+}
+
+/// draw image
+Future<int?> drawBitmap({required Bitmap bitmap, required int xDest, required int yDest}){
+  throw UnimplementedError('drawBitmap({required Bitmap bitmap, required int xDest, required int yDest}) has not been implemented');
+}
+
+
+/// draw image ex
+  Future<int?> drawBitmapEx({required List<int> bytes, required int xDest, required int yDest,required int widthDest, required int heightDest}){
+    throw UnimplementedError('drawBitmapEx({required List<int> bytes, required int xDest, required int yDest,required int widthDest, required int heightDest}) has not been implemented');
+  }
+
+
+  /// prn_open
+Future<int?> prnOpen(){
+  throw UnimplementedError('prnOpen() has not been implemented');
+
+}
+
+/// prn_close
+Future<int?> prnClose(){
+  throw UnimplementedError('prnClose() has not been implemented');
+
+}
+
+/// prn_setBlack
+Future<int?> prnSetBlack(int level){
+  throw UnimplementedError('prnSetBlack(int level) has not been implemented');
+
+}
+
+///prn_paperForWard
+Future<int?> prnPaperForWard(int length){
+  throw UnimplementedError('prnPaperForWard(int length) has not been implemented');
+}
+
+/// prn_paperBack
+  Future<int?> prnPaperBack(int length){
+    throw UnimplementedError('prnPaperForWard(int length) has not been implemented');
+  }
+
+///   prn_setSpeed
+///   sets printer speed [0 - 9]
+Future<int?> prnSetSpeed(int level){
+  throw UnimplementedError('prnSetSpeed(int level) has not been implemented');
+}
+
+///prn_getTemp
+/// gets temperature of printer
+  Future<int?> prnGetTemp(){
+    throw UnimplementedError('prnGetTemp() has not been implemented');
+  }
+
+///prn_setupPage
+///set up page with width and height
+  Future<int?> prnSetupPage({required int width, required int height}){
+    throw UnimplementedError('prnSetupPage({required int width, required int height}) has not been implemented');
+  }
+/// prn_clearPage
+  /// clears page
+  Future<int?> prnClearPage(){
+    throw UnimplementedError('prnClearPage() has not been implemented');
+  }
+
+///prn_printPage
+///prints page
+  Future<int?> prnPrintPage(){
+    throw UnimplementedError('prnPrintPage() has not been implemented');
+  }
+
+
+  ///prn_drawLine
+  /// draw lines
+  Future<int?> prnDrawLine({required int x0, required int y0, required int  x1, required int y1, required int lineWidth}){
+    throw UnimplementedError('prnDrawLine({required int x0, required int y0, required int  x1, required int y1, required int lineWidth})has not been implemented');
+  }
+
+/// prn_drawText
+/// draws text
+  Future<int?> prnDrawText({required String data, required int x, required int y, required String fontName, required bool isBold, required bool isItalic, required int rotate }){
+    throw UnimplementedError('prnDrawText({required String data, required int x, required int y, required String fontName, required bool isBold, required bool isItalic, required int rotate }) has not been implemented');
+  }
+
+///prn_drawTextEx
+  ///
+  Future<int?> prnDrawTextEx({required String data, required int x, required int y,required int width, required int height, required String fontName,required int fontSize, required bool isBold, required bool isItalic, required int rotate, required int style,required int format }){
+    throw UnimplementedError('prnDrawTextEx({required String data, required int x, required int y,required int width, required int height, required String fontName,required int fontSize, required bool isBold, required bool isItalic, required int rotate, required int style,required int format }) has not been implemented');
+  }
+
+/// prn_drawBarcode
+  Future<int?> prnDrawBarcode({required String data, required int x, required int y, required int barcodeType, required int width, required int height, required int rotate}){
+    throw UnimplementedError('prnDrawBarcode({required String data, required int x, required int y, required int barcodeType, required int width, required int height, required int rotate}) has not been implemented');
+  }
+
+  /// prn_drawBarcode
+  Future<int?> prnDrawBitmap({required Bitmap bitmap, required int xDest, required int yDest}){
+    throw UnimplementedError('prnDrawBitmap({required Bitmap bitmap, required int xDest, required int yDest}) has not been implemented');
+  }
+
+  /// prn_getStatus
+  Future<int?> prnGetStatus(){
+    throw UnimplementedError('prnGetStatus() has not been implemented');
+  }
+
+  /// getTemp
+  Future<int?> getTemp(){
+    throw UnimplementedError('getTemp() has not been implemented');
+  }
+
+  /// printCachedPage
+  Future<int?> printCachedPage(){
+    throw UnimplementedError('printCachedPage() has not been implemented');
+  }
+
+
+
 }

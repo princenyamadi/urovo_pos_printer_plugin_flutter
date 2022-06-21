3262ceb456d4b4dee40db1fb00e17025047e63ee
= <=>
2022-06-21 08:06:32 +0000

    added utility class

---
+++ lib/util/util.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+
+class Utility {
+  static String getDateTime() {
+    DateTime now = DateTime.now();
+    return now.toString();
+  }
+
+  static Future<int?> getBitmap(ByteData imageBytes) async {
+    List<int> values = imageBytes.buffer.asUint8List();
+    img.Image? photo;
+    photo = img.decodeImage(values);
+    int pixel = photo!.getPixel(5, 0);
+    return pixel;
+  }
+}
